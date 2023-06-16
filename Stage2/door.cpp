e1fc53d75784c878288cf45976e1016e2c3c7da9
12oina0eno9ionsa <61332704+12oina0eno9ionsa@users.noreply.github.com>
2023-06-15 20:32:20 -0400

    Add files via upload

---
+++ Stage2/door.cpp
@@ -0,0 +1,17 @@
+#include "door.h"
+
+Door::Door()
+{
+
+}
+Door::Door(int zone): magneticSensor(zone) {
+}
+void Door::changeState() {
+    if (magneticSensor.isClose())
+    {
+        magneticSensor.setSensorOpen();
+    }
+    else
+        magneticSensor.setSensorClose();
+}
+
